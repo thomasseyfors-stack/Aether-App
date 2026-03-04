72c744d32bc9fe209044a546979fedd313dde36b
Thomas Seyfors <thomas.seyfors@gmail.com>
2026-03-03 19:43:33 -0500

    Update main.tsx
    Signed-off-by: Thomas Seyfors <thomas.seyfors@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
